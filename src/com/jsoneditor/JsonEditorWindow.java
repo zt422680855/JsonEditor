214985b4a95958a8ab2c753436b3674dd9de7fe6
19043204 <19043204@cnsuning.com>
2020-09-11 10:46:49 +0800

    json editor

--- src/com/jsoneditor/JsonEditorWindow.java
+++ src/com/jsoneditor/JsonEditorWindow.java
@@ -1,13 +1,20 @@
 package com.jsoneditor;
 
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ex.ToolWindowEx;
+import com.intellij.ui.AnActionButton;
 import com.intellij.ui.components.JBPanel;
+import com.jsoneditor.actions.*;
 import com.jsoneditor.moddles.Left;
 import com.jsoneditor.moddles.Middle;
 import com.jsoneditor.moddles.Right;
 
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Description: 开发时iml文件module节点的type要等于PLUGIN_MODULE。
@@ -16,15 +23,17 @@ import java.awt.*;
  */
 public class JsonEditorWindow extends JBPanel {
 
-    public Left left;
+    private Left left;
 
-    public Middle middle;
+    private Middle middle;
 
-    public Right right;
+    private Right right;
 
-    public Project project;
+    private Project project;
 
-    public ToolWindow toolWindow;
+    private ToolWindow toolWindow;
+
+    private static Map<String, DefaultActionGroup[]> actionMap = new HashMap<>();
 
     public JsonEditorWindow(Project project, ToolWindow toolWindow) {
         this.project = project;
@@ -37,6 +46,39 @@ public class JsonEditorWindow extends JBPanel {
         middle.toRight(left, right);
         middle.toLeft(left, right);
         middle.syncToRight.doClick();
+        addActions();
+        setContext();
+    }
+
+    private void addActions() {
+        Format format = new Format(left);
+        Compress compress = new Compress(left);
+        Reset reset = new Reset(left);
+        DefaultActionGroup leftAction = new DefaultActionGroup(format, compress, reset);
+        leftAction.addSeparator();
+        Expand expand = new Expand(right);
+        Close close = new Close(right);
+        Back back = new Back();
+        Forward forward = new Forward();
+        DefaultActionGroup rightAction = new DefaultActionGroup(expand, close, back, forward);
+        rightAction.addSeparator();
+        DefaultActionGroup otherAction = new DefaultActionGroup(new SwitchView(right, middle));
+        DefaultActionGroup[] actions = new DefaultActionGroup[]{leftAction, rightAction, otherAction};
+        actionMap.put(project.getName(), actions);
+    }
+
+    public void setContext() {
+        DefaultActionGroup[] groups = actionMap.get(project.getName());
+        ToolWindowEx ex = (ToolWindowEx) toolWindow;
+        ex.setTitleActions(groups);
+        for (DefaultActionGroup g : groups) {
+            AnAction[] children = g.getChildren(null);
+            for (AnAction child : children) {
+                if (child instanceof AnActionButton) {
+                    ((AnActionButton) child).setContextComponent(this);
+                }
+            }
+        }
     }
 
 }
