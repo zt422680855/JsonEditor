0e2a80aeaf9fef9fcc89d6182a4cb4fbd3b58468
19043204 <19043204@cnsuning.com>
2020-09-11 16:42:04 +0800

    json editor

--- src/com/jsoneditor/actions/CopyToEditor.java
+++ src/com/jsoneditor/actions/CopyToEditor.java
@@ -1,5 +1,8 @@
 package com.jsoneditor.actions;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -7,10 +10,9 @@ import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiField;
-import com.intellij.psi.PsiFile;
+import com.intellij.psi.*;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.PsiShortNamesCache;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 
@@ -21,9 +23,12 @@ import org.jetbrains.annotations.NotNull;
  */
 public class CopyToEditor extends AnAction {
 
+    private Project project;
+
     private ToolWindow toolWindow;
 
-    public CopyToEditor(ToolWindow toolWindow) {
+    public CopyToEditor(Project project, ToolWindow toolWindow) {
+        this.project = project;
         this.toolWindow = toolWindow;
     }
 
@@ -33,14 +38,57 @@ public class CopyToEditor extends AnAction {
         Editor editor = ctx.getData(CommonDataKeys.EDITOR);
         PsiFile psiFile = ctx.getData(CommonDataKeys.PSI_FILE);
         if (editor != null && psiFile != null) {
-            Project project = editor.getProject();
+//            Project project = editor.getProject();
             PsiElement selectElement = psiFile.findElementAt(editor.getCaretModel().getOffset());
             PsiClass selectClass = PsiTreeUtil.getContextOfType(selectElement, PsiClass.class);
             if (selectClass != null) {
-                PsiField[] fields = selectClass.getAllFields();
+                JSONObject object = generateObj(selectClass);
 
                 toolWindow.show(null);
             }
         }
+    }
+
+    private JSONObject generateObj(PsiClass psiClass) {
+        JSONObject obj = new JSONObject(true);
+        PsiField[] fields = psiClass.getAllFields();
+        for (PsiField field : fields) {
+            String name = field.getName();
+            PsiType type = field.getType();
+            String typeName = type.getPresentableText();
+            if (type instanceof PsiClassType) {
+                PsiClassType classType = (PsiClassType) type;
+                String className = classType.getClassName();
+                obj.put(name, getObjByClassName(className));
+            } else if (type instanceof PsiArrayType) {
+                PsiClass containingClass = field.getContainingClass();
+                if (containingClass != null) {
+                    obj.put(name, new JSONArray() {{
+                        add(generateObj(containingClass));
+                    }});
+                }
+            }
+        }
+        return obj;
+    }
+
+    private Object getObjByClassName(String className) {
+        try {
+            Class<?> aClass = Class.forName(className);
+            return aClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private PsiClass getPsiClassByShortClassName(String shortClassName) {
+        PsiClass[] classes = PsiShortNamesCache.getInstance(project)
+                .getClassesByName(shortClassName, GlobalSearchScope.projectScope(project));
+        for (PsiClass psiClass : classes) {
+            PsiJavaFile javaFile = (PsiJavaFile) psiClass.getContainingFile();
+            String packageName = javaFile.getPackageName();
+        }
+        return null;
     }
 }
